e4459025f969cdfa43d5c379ff3c305870d771ff
Jacques Aucamp <jacquesaucamp99@gmail.com>
2026-02-16 20:02:17 +0200

    Unicafe done - Adds feedback statistics display and handling
    Introduces separate buttons for each feedback type
    and a statistics section showing counts, average,
    and positive percentage. Improves user experience
    by summarizing feedback and handling cases with no
    feedback.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,15 +1,82 @@
 import { useState } from 'react'
 
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const StatisticLine = (props) => {
+  return (
+    // Place the statistics in a table, with the text in the left column and the value in the right column
+    // Also make the columns align to the left and make the columns width 200px
+    <table style={{ textAlign: 'left', tableLayout: 'fixed', width: '200px' }}>
+      <tbody>
+        <tr>
+          <td>{props.text}</td>
+          <td>{props.value}</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+const Statistics = (props) => {
+  const good = props.good
+  const neutral = props.neutral
+  const bad = props.bad
+  const all = props.allClicks
+  
+  return (
+    // if no feedback is given, display "No feedback given"
+    all === 0 ? <p>No feedback given</p> :
+      
+      <div>
+        <h1> Statistics </h1>
+        <StatisticLine text='good' value={good}/>
+        <StatisticLine text='neutral' value={neutral}/>
+        <StatisticLine text='bad' value={bad}/>
+        <StatisticLine text='all' value={all}/>
+        <StatisticLine text='average' value={(good - bad) / all}/>
+        <StatisticLine text='positive' value={(good / all) * 100}/>
+      </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState(0)
+
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setAll(allClicks + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setAll(allClicks + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setAll(allClicks + 1)
+  }
 
   return (
     <div>
-      code here
+      <h1> Give Feedback </h1>
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+
+      <Statistics good={good} neutral={neutral} bad={bad} allClicks={allClicks}/>
     </div>
+
+    
   )
 }
 
